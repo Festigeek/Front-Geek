6357d5e8b97377c7919f03ec986a055b39d10bf7
Pierre-Alain Curty <pacurty@gmail.com>
2016-02-10 18:48:55 +0100

    First commit of Yeoman's AngularJS project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fgfrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fgfrontEndApp
+ */
+angular.module('fgfrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fgfrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fgfrontEndApp
+ */
+angular.module('fgfrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
