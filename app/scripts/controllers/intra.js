56e1ab5d4b8549ccceb4da2607f1b50c6943f87f
Mysteriosis <pacurty@gmail.com>
2017-05-25 12:10:15 +0200

    Add intra pages, fix dropdown-menu on mobile, update profile

---
+++ app/scripts/controllers/intra.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontGeekApp.controller:IntraCtrl
+ * @description
+ * # IntraCtrl
+ * Controller of the frontGeekApp
+ */
+angular.module('frontGeekApp')
+  .controller('IntraCtrl', function () {
+    // ...
+  });
