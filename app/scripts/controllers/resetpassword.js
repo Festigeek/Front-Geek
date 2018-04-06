ae91cff0169270d7af2d1dd4d3353d6fcc889379
Mysteriosis <pacurty@gmail.com>
2018-04-06 15:04:12 +0200

    Add redirect after password reset

--- app/scripts/controllers/resetpassword.js
+++ app/scripts/controllers/resetpassword.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('frontGeekApp')
-  .controller('ResetPasswordCtrl', function ($scope, $log, $stateParams, User, toastr, ngDialog, resetToken, resetMail) {
+  .controller('ResetPasswordCtrl', function ($scope, $log, $state, $stateParams, User, toastr, ngDialog, resetToken, resetMail) {
     $scope.user = {
       'token': resetToken,
       'email': resetMail
@@ -11,6 +11,7 @@ angular.module('frontGeekApp')
       newPass.$resetPassword($scope.resetInfos, function() {
         toastr.success('Ton mot de passe a été réinitialisé. Tu peu désormais te reconnecter.', 'Réinitialisation réussie !');
         ngDialog.closeAll();
+        $state.go('/');
       }, function() {
         toastr.error('Erreur lors de la réinitialisation du mot de passe...', 'Erreur !');
       });
