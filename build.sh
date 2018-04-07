95c65f00d513b28a1c3e469d2bca8a7adb50a672
Mysteriosis <pacurty@gmail.com>
2018-04-07 17:20:39 +0200

    Delete build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mv /var/www/maintenance/festigeek.ch/maintenance_off.html /var/www/maintenance/festigeek.ch/maintenance_on.html
-
-grunt build
-
-mv /var/www/maintenance/festigeek.ch/maintenance_on.html /var/www/maintenance/festigeek.ch/maintenance_off.html
