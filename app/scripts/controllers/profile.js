4ebd68b281cf505b67314966e9927e81a31ec533
Mysteriosis <pacurty@gmail.com>
2018-04-16 21:44:33 +0200

    Update Profile

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,7 +8,7 @@
  * Controller of the frontGeekApp
  */
 angular.module('frontGeekApp')
-  .controller('ProfileCtrl', function ($log, urls, $scope, $auth, $location, $localStorage, toastr, User, Country, Order, Team) {
+  .controller('ProfileCtrl', function ($log, $window, $http, urls, $scope, $auth, $location, $localStorage, toastr, User, Country, Order, Team) {
     $scope.subPage = 1;
 
     $scope.$storage = $localStorage;
@@ -33,14 +33,39 @@ angular.module('frontGeekApp')
           $log.log(team); //TODO if null
         });
       });
-
-
     });
 
+    // Download du ticket en PDF
     $scope.getTicket = function() {
-      window.location.href = urls.BASE_API + '/orders/' + $scope.orders[0].id + '?format=pdf&token=' + $auth.getToken();
+      $http({
+        method: 'GET', 
+        url: urls.BASE_API + '/orders/' + $scope.orders[0].id + '?format=pdf',
+        headers : {
+          'Content-type' : 'application/pdf'
+        },
+        responseType : 'arraybuffer'})
+        .then(function(res) {
+          $log.log(res);
+          var pdfFile = new Blob([ res.data ], {
+            type : 'application/pdf'
+          });
+
+          $window.open(URL.createObjectURL(pdfFile));
+          // var anchor = angular.element('<a/>');
+          // var header = 'data:application/octet-binary;charset=utf-8,';
+
+          // anchor.attr({
+          //   href: header + encodeURI(data),
+          //   target: '_blank',
+          //   download: 'ticket_festigeek_2018.pdf'
+          // })[0].click();
+        })
+        .catch(function(data){
+          $log.error(data);
+        });
     };
 
+    // Copy team code in clipboard
     // $scope.copyCode = function() {
     //   var $temp = $('h3');
     //   $('body').append($temp);
@@ -74,6 +99,69 @@ angular.module('frontGeekApp')
 
     };
 
+    $scope.checkLastMail = function() {
+      return ($localStorage.lastMail === undefined || (new Date()).getTime() > $localStorage.lastMail);
+    };
+
+    $scope.getBankTransfertMail = function() {
+      if($scope.checkLastMail()) {
+        $http({
+            method: 'GET',
+            url: urls.BASE_API + '/orders/' + $scope.orders[0].id + '/getPayment?paymentType=1'
+          })
+          .then(function(res) {
+            $log.log(res);
+            toastr.success('Mail envoyé !');
+            $localStorage.lastMail = (new Date()).getTime() + 900000;
+          })
+          .catch(function(data){
+            toastr.warning('Une erreur est survenue, merci de contacter un admin.');
+            $log.error(data);
+          });
+      }
+      else {
+        var time = (new Date($localStorage.lastMail - (new Date()).getTime())).getMinutes();
+        toastr.warning("Merci d'attendre " + time + ' minutes avant de réessayer.');
+      }
+    };
+
+
+    $scope.getPaypalInvoice = function() {
+      $http({
+        method: 'GET', 
+        url: urls.BASE_API + '/orders/' + $scope.orders[0].id + '/getPayment?paymentType=2'
+      })
+      .then(function(res) {
+        $log.log(res);
+        $window.open(res.data.link, '_self');
+      })
+      .catch(function(data){
+        toastr.warning('Une erreur est survenue, merci de contacter un admin.');
+        $log.error(data);
+      });
+    };
+
+    // ADMIN
+
+    $scope.getAllOrders = function(format) {
+      $http({method: 'GET', url: urls.BASE_API + '/orders?format=' + format})
+        .then(function(res) {
+          $log.log(res);
+          var anchor = angular.element('<a/>');
+          var header = (format === 'csv') ? 'data:attachment/csv;charset=utf-8,' : 'data:attachment/json;charset=utf-8,';
+          var href = (format === 'csv') ? header + encodeURI( res.data ) : header + encodeURI(JSON.stringify(res.data ));
+
+          anchor.attr({
+            href: href,
+            target: '_blank',
+            download: 'orders.' + format
+          })[0].click();
+        })
+        .catch(function(data){
+          $log.error(data);
+        });
+    };
+
     $scope.generateStats = function() {
       $scope.orderStats = Order.getByEvent(2, function(orders){
         $scope.testingmotherfuckingShit = orders;
@@ -81,8 +169,5 @@ angular.module('frontGeekApp')
         $log.log(log);
       });
     };
-
-
-
 
   });
